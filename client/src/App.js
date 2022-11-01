fd721fe9ea8bf497a645f5d2902a9d159055c026
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-11-01 13:00:18 +0200

    routing added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import { ToastContainer } from "react-toastify";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <ToastContainer />
-      <h2>App</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
